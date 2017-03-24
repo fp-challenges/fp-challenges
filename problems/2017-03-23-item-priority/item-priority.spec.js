c8591ecb747ffc42ffdb78d3cad8cc517550ef74
Nghe, Victor <Victor.Nghe@LibertyMutual.com>
2017-03-24 09:34:49 -0400

    Making item-priority tests a little more helpful with mocha

--- problems/2017-03-23-item-priority/item-priority.spec.js
+++ problems/2017-03-23-item-priority/item-priority.spec.js
@@ -42,34 +42,53 @@ const items2 = [
   }
 ]
 
-expect(sut(items, ['Action', 'Drama', 'Comedy', 'Musical', 'Documentary'])).to.deep.equal({
-  name : 'The Avengers',
-  category : 'Action'
-}, 'Avengers');
+describe('The item-priority test', function() {
 
-expect(sut(items, ['Drama', 'Action', 'Comedy', 'Musical', 'Documentary'])).to.deep.equal({
-  name : 'Good Will Hunting',
-  category : 'Drama'
-}, 'GWH');
+  it('should return an action movie if an action movie is provided and action is most preferred', function() {
+    expect(sut(items, ['Action', 'Drama', 'Comedy', 'Musical', 'Documentary'])).to.deep.equal({
+      name : 'The Avengers',
+      category : 'Action'
+    }, 'Avengers');
+  });
+  
+  it('should return a drama movie if a drama movie is provided and drama is most preferred', function() {
+    expect(sut(items, ['Drama', 'Action', 'Comedy', 'Musical', 'Documentary'])).to.deep.equal({
+      name : 'Good Will Hunting',
+      category : 'Drama'
+    }, 'GWH');
+  });
 
-expect(sut(items, ['Documentary', 'Drama', 'Action', 'Comedy', 'Musical'])).to.deep.equal({
-  name : 'Food, Inc.',
-  category : 'Documentary'
-}, 'Food Inc.');
+  it('should return a documentary movie if a documentary movie is provided and documentary is most preferred', function() {
+    expect(sut(items, ['Documentary', 'Drama', 'Action', 'Comedy', 'Musical'])).to.deep.equal({
+      name : 'Food, Inc.',
+      category : 'Documentary'
+    }, 'Food Inc.');
+  });
 
-expect(sut(items, ['Romantic Comedy', 'Musical', 'Action', 'Drama', 'Comedy', 'Documentary'])).to.deep.equal({
-  name : 'Les Miserables',
-  category : 'Musical'
-}, 'Les Miserables');
+  it('should return a musical movie if a musical movie is provided and the most preferred movie category is not matched,\
+    but musical is more preferred than other available categories', function() {
+    expect(sut(items, ['Romantic Comedy', 'Musical', 'Action', 'Drama', 'Comedy', 'Documentary'])).to.deep.equal({
+      name : 'Les Miserables',
+      category : 'Musical'
+    }, 'Les Miserables');
+  });
 
-expect(sut(items2, categories)).to.deep.equal({
-  name : 'The Human Centipede',
-  category : 'Cringe Horror'
-}, 'The Human Centipede (yuck)');
+  it('should return the first movie provided if no preferred movie categories are matched', function() {
+    expect(sut(items2, categories)).to.deep.equal({
+      name : 'The Human Centipede',
+      category : 'Cringe Horror'
+    }, 'The Human Centipede (yuck)');
+  });
 
-expect(sut(items2, [])).to.deep.equal({
-  name : 'The Human Centipede',
-  category : 'Cringe Horror'
-}, 'The Human Centipede (yuck)');
+  it('should return the first movie provided if no categories are provided', function() {
+    expect(sut(items2, [])).to.deep.equal({
+      name : 'The Human Centipede',
+      category : 'Cringe Horror'
+    }, 'The Human Centipede (yuck)');
+  });
 
-expect(sut([], categories)).to.equal(undefined, 'Cannot select a movie when none given');
+  it('should return undefined if no movies are provided', function() {
+    expect(sut([], categories)).to.equal(undefined, 'Cannot select a movie when none given');
+  });
+
+});
